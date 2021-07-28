c2f6f476b3cf3d65ab7c11ae62a2189b16a69795
Matin Najafi <matin.najafi@gmail.com>
2021-07-28 21:36:41 +0430

    Create __init__.py

---
+++ source/__init__.py
@@ -0,0 +1,12 @@
+
+CATEGORIES = {
+    'Web application framework Software': {
+        'Django': 'https://github.com/django/django/tree/main/docs',
+        'Flask': 'https://github.com/pallets/flask/tree/main/docs',
+        'Bottle': 'https://github.com/bottlepy/bottle/tree/master/docs'
+    },
+    'Mobile app development': {
+        'Swift': 'https://github.com/apple/swift/tree/main/docs'
+    },
+
+}
