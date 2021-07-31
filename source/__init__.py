b1180b8df87431397926e5f5cbef4bfc627d4309
Matin Najafi <matin.najafi@gmail.com>
2021-07-31 12:09:20 +0430

    Add some url and comments in initial file

--- source/__init__.py
+++ source/__init__.py
@@ -1,13 +1,20 @@
 tabs = (' '*22)
 
+# Here are the different categories available in this tool
+# And the folder address of the documentation for those projects must be entered in the GitHub.
+
 CATEGORIES = {
     'Web application framework Software': {
         'Django': 'https://github.com/django/django/tree/main/docs',
         'Flask': 'https://github.com/pallets/flask/tree/main/docs',
-        'Bottle': 'https://github.com/bottlepy/bottle/tree/master/docs'
+        'Bottle': 'https://github.com/bottlepy/bottle/tree/master/docs',
+        'React Native': 'https://github.com/facebook/react-native/tree/main/docs'
     },
     'Mobile app development': {
         'Swift': 'https://github.com/apple/swift/tree/main/docs'
     },
-
+    'Databases': {
+        'Mongo': 'https://github.com/mongodb/mongo/tree/master/docs',
+        'PostgreSQL': 'https://github.com/postgres/postgres/tree/master/doc'
+    }
 }
